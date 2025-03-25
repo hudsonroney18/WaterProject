7e3cc11e84518707dddc79daed2c3f1538a58700
hudsonroney18 <hudsonbroney@gmail.com>
2025-03-24 20:48:01 -0600

    Final Change for first vids

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import ProjectList from './ProjectList'
 import CookieConsent from 'react-cookie-consent';
